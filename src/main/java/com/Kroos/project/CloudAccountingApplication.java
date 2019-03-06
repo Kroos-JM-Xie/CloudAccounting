e53b3309a56eb0d673ffdfd61491cb685fe9f5e5
Kroos-JM-Xie <xjmchris03@163.com>
2019-03-06 22:42:40 +0800

    Linux初次使用intellij

--- src/main/java/com/Kroos/project/CloudAccountingApplication.java
+++ src/main/java/com/Kroos/project/CloudAccountingApplication.java
@@ -2,7 +2,6 @@ package com.Kroos.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
